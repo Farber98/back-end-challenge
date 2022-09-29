16e8968471b295a549c223898a62368a14c5f09f
Farber98 <juantuc98@gmail.com>
2022-09-29 14:59:58 -0300

    all endpoints working. test time.

--- internal/controllers/ProductsController.go
+++ internal/controllers/ProductsController.go
@@ -9,6 +9,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	ERR_PARTIAL_SUCCESS = "ERROR. Successful inserts."
+	ERR_PARTIAL_FAILURE = "ERROR. Failed inserts."
+	OK_INSERTS_SUCCESS  = "Succesful inserts."
+	ERR_ID_FORMAT       = "Wrong ID format."
+	OK_SEARCH_SUCCESS   = "Succesful search."
+	ERR_DEFAULT         = "ERROR. Please, call the administrator."
+)
+
 type ProductsController struct {
 	services.ProductsService
 }
@@ -23,7 +32,7 @@ func (controller *ProductsController) BatchInsert(c echo.Context) error {
 	productJSON := models.ProductsJSON{}
 
 	if err := c.Bind(&productJSON); err != nil {
-		return c.JSON(http.StatusBadRequest, models.NewResponseWithoutData(err.Error()))
+		return c.JSON(http.StatusBadRequest, models.NewResponseWithoutData(ERR_DEFAULT))
 	}
 
 	var FailedProducts []*models.FailedProduct
@@ -48,38 +57,38 @@ func (controller *ProductsController) BatchInsert(c echo.Context) error {
 		// Tags insertion
 		_, err = controller.ProductsService.InsertTags(product)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, models.NewResponseWithData("ERROR. Successful inserts.", SuccededProducts))
+			return c.JSON(http.StatusInternalServerError, models.NewResponseWithData(ERR_PARTIAL_SUCCESS, SuccededProducts))
 		}
 
 		SuccededProducts = append(SuccededProducts, dbProduct.Product)
 	}
 
 	if FailedProducts != nil || len(FailedProducts) > 0 {
-		return c.JSON(http.StatusMultiStatus, models.NewResponseWithData("ERROR. Failed.", FailedProducts))
+		return c.JSON(http.StatusMultiStatus, models.NewResponseWithData(ERR_PARTIAL_FAILURE, FailedProducts))
 	}
 
-	return c.JSON(http.StatusOK, models.NewResponseWithData("Succesful inserts.", SuccededProducts))
+	return c.JSON(http.StatusOK, models.NewResponseWithData(OK_INSERTS_SUCCESS, SuccededProducts))
 }
 
 func (controller *ProductsController) Search(c echo.Context) error {
+	var idInt uint64
+	var err error
 
 	prodId := c.QueryParam("id")
 	prodName := c.QueryParam("name")
 	prodTag := c.QueryParam("tag")
 
-	var idInt uint64
-	var err error
 	if prodId != "" {
 		idInt, err = strconv.ParseUint(prodId, 10, 32)
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, models.NewResponseWithoutData(err.Error()))
+			return c.JSON(http.StatusBadRequest, models.NewResponseWithoutData(ERR_ID_FORMAT))
 		}
 	}
 	search := &models.Product{IDProduct: uint16(idInt), Product: prodName, Tag: prodTag}
 	resp, err := controller.ProductsService.Search(search)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, models.NewResponseWithoutData(err.Error()))
+		return c.JSON(http.StatusInternalServerError, models.NewResponseWithoutData(ERR_DEFAULT))
 	}
 
-	return c.JSON(http.StatusOK, models.NewResponseWithData("Search results.", resp))
+	return c.JSON(http.StatusOK, models.NewResponseWithData(OK_SEARCH_SUCCESS, resp))
 }
